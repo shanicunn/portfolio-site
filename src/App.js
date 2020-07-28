7fe17306e82162e527063fa4e2c617ef8e4c6e4b
Shani Cunnnigham <shanicunnnigham@MacBook-Air-8.local>
2020-07-28 14:42:32 -0400

    Site structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import {Header} from './components/Header';
+import {Nav} from './components/Nav';
+import { AboutMe } from './components/AboutMe';
+import { Portfolio } from './components/Portfolio';
+import {Services} from './components/Services';
+import { Footer } from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+
+      <Nav />
+    <div className="about-services">
+      <AboutMe />
+      <Services /> 
+    </div>
+      <Portfolio />
+      <Footer />     
     </div>
   );
 }
